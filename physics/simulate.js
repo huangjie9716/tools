e61f51a5365b7121c1c5f0392af27f5df7337913
huangjie9716 <71445619+huangjie9716@users.noreply.github.com>
2026-08-20 15:16:33 +0800

    [skip netlify]更新部分软件

--- physics/simulate.js
+++ physics/simulate.js
@@ -26,23 +26,23 @@ var simLinks = {
 
     '研究光的折射现象': 'https://wl.nobook.com/physics/ce7fc8e03ad12f13c7432dfbd35ef223?moduleId=2&is_resource_show=1&title=%E7%A0%94%E7%A9%B6%E5%85%89%E7%9A%84%E6%8A%98%E5%B0%84%E7%8E%B0%E8%B1%A1&returnUrl=%2Fconsole%2Ftemplates%2Fresource&exp_id=183',
     //八上第五章
-
+    '探究凸透镜成像的规律': 'experiment/eight5A4.html',
     //八上第六章
-
+    '用托盘天平测量物体的质量': 'experiment/eight6A1.html',
     //八下第七章
-     '用弹簧测力计测量力':'https://wl.nobook.com/physics/5c756fb10952d2f581676cf6c76ce8f8?moduleId=5&is_resource_show=1&title=%E7%BB%83%E4%B9%A0%E4%BD%BF%E7%94%A8%E5%BC%B9%E7%B0%A7%E6%B5%8B%E5%8A%9B%E8%AE%A1&returnUrl=%2Fconsole%2Ftemplates%2Fresource&exp_id=156',
-     '探究重力的大小跟质量的关系':'https://wl.nobook.com/physics/73ab0640-3327-11e9-9bcd-fd6afe6dc467?moduleId=5&is_resource_show=1&title=%E6%8E%A2%E7%A9%B6%E9%87%8D%E5%8A%9B%E7%9A%84%E5%A4%A7%E5%B0%8F%E8%B7%9F%E8%B4%A8%E9%87%8F%E7%9A%84%E5%85%B3%E7%B3%BB&returnUrl=%2Fconsole%2Ftemplates%2Fresource&exp_id=1865',
+     '用弹簧测力计测量力':'',
+     '探究重力的大小跟质量的关系':'',
     //八下第八章
-
-    '探究二力平衡的条件':"https://wl.nobook.com/physics/a25e31b6f00f93bda29fb9f09d19f09e?moduleId=5&is_resource_show=1&title=%E6%8E%A2%E7%A9%B6%E4%BA%8C%E5%8A%9B%E5%B9%B3%E8%A1%A1%E7%9A%84%E6%9D%A1%E4%BB%B6&returnUrl=%2Fconsole%2Ftemplates%2Fresource&exp_id=143",
+    '阻力对物体运动的影响': 'experiment/eight8A1.html',
+    '探究二力平衡的条件':"",
      '探究滑动摩擦力的大小与哪些因素有关': 'experiment/eight8A3.html',
     //八下第九章
 
     //八下第十章
-
-    '探究浮力的大小与排开液体所受重力的关系':'https://wl.nobook.com/physics/6152c0db22a16db3cb01d2b52be3cd68?moduleId=5&is_resource_show=1&title=%E6%8E%A2%E7%A9%B6%E6%B5%AE%E5%8A%9B%E7%9A%84%E5%A4%A7%E5%B0%8F%E4%B8%8E%E6%8E%92%E5%BC%80%E6%B6%B2%E4%BD%93%E6%89%80%E5%8F%97%E9%87%8D%E5%8A%9B%E7%9A%84%E5%85%B3%E7%B3%BB&returnUrl=%2Fconsole%2Ftemplates%2Fresource&exp_id=132',
+    '潜水艇的工作原理': 'experiment/eight10A5.html',
+    '探究浮力的大小与排开液体所受重力的关系':'',
     //八下第十一章
-
+    '动能和势能的转化': 'experiment/eight11A2.html',
     //八下第十二章
 
     //九年级第十三章
@@ -50,7 +50,7 @@ var simLinks = {
     //九年级第十四章
      '热机工作原理':'https://wl.nobook.com/physics/09be2259345bcfaa4aaab3724fc7ccc3?moduleId=4&is_resource_show=1&title=%E5%9B%9B%E5%86%B2%E7%A8%8B%E6%B1%BD%E6%B2%B9%E6%9C%BA&returnUrl=%2Fconsole%2Ftemplates%2Fresource&exp_id=171',
     //九年级第十五章
-
+     '摩擦起电的原因': 'experiment/eight15A3.html',
     //九年级第十六章
 
     //九年级第十七章
@@ -61,6 +61,8 @@ var simLinks = {
 
     //九年级第二十章
 
+   '电动机工作原理': 'https://5b9scx48.html2web.com/',
+   '发电机工作原理': 'https://lg7uf1tv.html2web.com/',
     //九年级第二十一章
 
     //九年级第二十二章
