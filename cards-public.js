1d22f06b7738d642db4f8f6cd8a8a54d35e38e45
huangjie9716 <71445619+huangjie9716@users.noreply.github.com>
2026-09-01 10:05:22 +0800

    [skip netlify]

--- cards-public.js
+++ cards-public.js
@@ -14,5 +14,15 @@ window.CARDS_PUBLIC = [
         templates: [
             { label: '学生数据模板', url: 'https://github.com/huangjie9716/tools/releases/download/v1.0/studentinfo2.xlsx' }
         ]
+    },
+      {
+        title: '公开课倒计时系统',
+        desc: '公开课自定义倒计时，可视化时间管理。',
+        date: '2026.09.01',
+        version: 'v1.1',
+        file: 'opencourse/2/CountDown.html',
+        href: 'opencourse/2/CountDown.html',
+        action: 'link',
+        actionText: '立即使用',
     }
 ];
