38e5d445c5be4a033ec082fd257728f6d9d20e9c
huangjie9716 <71445619+huangjie9716@users.noreply.github.com>
2026-09-17 12:05:20 +0800

    [skip netlify]Update physics teaching card data
    Updated the physics teaching card data with new versions and dates.

--- cards-physics.js
+++ cards-physics.js
@@ -17,8 +17,8 @@ window.CARDS_PHYSICS = [
     {
         title: '初中物理课堂知识PK赛',
         desc: '两人对战模式，涵盖初中物理课堂核心知识点，增强课堂互动与趣味性。',
-        date: '2026.08.04',
-        version: 'v1.1',
+        date: '2026.09.17',
+        version: 'v1.2',
         file: 'physics/1/PhysicsClassGame.html',
         href: 'physics/1/PhysicsClassGame.html',
         action: 'link',
@@ -28,8 +28,8 @@ window.CARDS_PHYSICS = [
     {
         title: '初中物理实验资源库',
         desc: '涵盖初中物理演示实验和学生实验，提供视频资源、模拟软件、习题资源，致力成为老师和学生们宝贵的学习财富！',
-        date: '2026.08.04',
-        version: 'v1.1',
+        date: '2026.09.17',
+        version: 'v1.2',
         file: 'physics/2/PhysicsExperiment.html',
         href: 'physics/2/PhysicsExperiment.html',
         action: 'link',
